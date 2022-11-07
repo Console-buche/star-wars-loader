ba6f36806bc96d5f392fa1e20a970a705f4e0234
sdubourg <sdubourg@tenable.com>
2022-11-08 00:28:05 +0100

    Cleanup

--- src/components/scene.tsx
+++ src/components/scene.tsx
@@ -2,21 +2,13 @@ import { OrbitControls, Stats } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Ambient } from "./ambient/ambient";
 import { Cam } from "./cam";
-import { Dust } from "./dust/dust";
 import { Ground } from "./ground";
-import { Loading } from "./Loading/Loading";
-import { MountainRidges } from "./mountainridge";
-import { XWing } from "./xwing";
 
 export const Scene = () => {
   return (
-    <Canvas flat shadows>
+    <Canvas>
       <Cam />
       <OrbitControls />
-      <XWing />
-      <Loading />
-      <MountainRidges animate />
-      {/* <Dust count={500} animate /> */}
       <Ground />
       <Ambient />
       <Stats />
