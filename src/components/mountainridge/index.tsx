2feda5201d46556aefa09f9d8974516ad2cd1ada
sdubourg <sdubourg@tenable.com>
2022-11-08 18:24:06 +0100

    demo-2

--- src/components/mountainridge/index.tsx
+++ src/components/mountainridge/index.tsx
@@ -6,36 +6,12 @@ type MountainRidges = {
 
 export const MountainRidges = ({ animate }: MountainRidges) => {
   return (
-    <group>
-      <MountainRidge
-        scrollSpeed={0.1}
-        chunkCount={30}
-        position={[-30, -2.95, 1]}
-        animate={animate}
-      />
-      <MountainRidge
-        scrollSpeed={0.25}
-        chunkCount={50}
-        position={[-30, -2.5, -10]}
-        scaleX={0.8}
-        animate={animate}
-      />
-      <MountainRidge
-        scrollSpeed={0.12}
-        chunkCount={50}
-        position={[-40, -6.5, -15]}
-        scaleX={1.5}
-        scaleY={3}
-        animate={animate}
-      />
-      <MountainRidge
-        scrollSpeed={0.06}
-        chunkCount={50}
-        position={[-50, -0.6, -20]}
-        scaleX={2.96}
-        scaleY={2.8}
-        animate={animate}
-      />
-    </group>
+    <MountainRidge
+      scrollSpeed={0.25}
+      chunkCount={50}
+      position={[-30, -2.5, -10]}
+      scaleX={0.8}
+      animate={animate}
+    />
   );
 };
