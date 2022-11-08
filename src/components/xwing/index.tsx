65ae3d854361468edc244eeb945838a42e3b4b75
sdubourg <sdubourg@tenable.com>
2022-11-08 18:27:40 +0100

    demo-3

--- src/components/xwing/index.tsx
+++ src/components/xwing/index.tsx
@@ -9,23 +9,5 @@ export const XWing = ({}: XWingProps) => {
   const { viewport } = useThree();
   const ref = useRef<Group>(null);
 
-  useFrame(({ clock, mouse }) => {
-    if (!ref.current) {
-      return;
-    }
-
-    const x = mouse.x * viewport.width;
-    const y = mouse.y * viewport.height;
-    ref.current.position.lerp(
-      new Vector3(x, y * 3, ref.current.position.z),
-      0.1
-    );
-    ref.current.rotation.x =
-      -x * 0.05 + Math.sin(clock.getElapsedTime()) * 0.25;
-    ref.current.position.y += Math.sin(clock.getElapsedTime()) * 0.025;
-    ref.current.position.z += Math.sin(clock.getElapsedTime()) * 0.025;
-
-    ref.current.position.y = MathUtils.clamp(ref.current.position.y, 0.75, 3.5);
-  });
   return <XwingModel ref={ref} position-z={-5} scale={0.25} />;
 };
